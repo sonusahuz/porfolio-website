1137fcbdf343c746341aa8df78aae07b0ba62d2c
Sonu Sahu <114206751+sonusahuz@users.noreply.github.com>
2023-12-31 14:21:35 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-export default function About({ showMenuBar, onClose }: any) {
+export default function About({ showMenuBar }: any) {
   return (
     <div
       id="about"
@@ -6,22 +6,27 @@ export default function About({ showMenuBar, onClose }: any) {
         showMenuBar ? "blur-sm" : ""
       } mb-12`}
     >
-      <div className="lg:w-[70%] about py-8 px-4 p-2 lg:p-10 lg:h-[480px] h-auto mt-20 lg:mt-10">
+      <div className="lg:w-[70%] about py-8 px-4 p-2 lg:p-10 lg:h-auto h-auto mt-20 lg:mt-10">
         <h1 className="font-extrabold mb-4 lg:text-5xl text-3xl text-center">
           About Me.
         </h1>
         <p className="py-2 lg:leading-9 leading-7 tracking-normal text-[16px] lg:text-[18px]">
-          Hello! My name is <span className="sonu font-medium">Sonu Sahu</span>
-          {} I have a great passion for creating and designing websites. I
-          started my web development career during my college days when I
-          developed a blog website utilizing HTML, CSS, and JavaScript. This
-          experience significantly amplified my interest in programming.
+          Hello! My name is <span className="sonu font-medium">Sonu Sahu</span>,
+          and I'm from Gurugram, Haryana. I have a great passion for creating
+          and designing websites. I began my web development journey during my
+          college days by creating a simple blog website using HTML, CSS, and
+          JavaScript.
+        </p>
+        <p className="py-2 lg:leading-9 leading-7 tracking-normal text-[16px] lg:text-[18px]">
+          I specialize in HTML, CSS, JavaScript, TypeScript, Tailwind CSS,
+          React.js, Node.js, Next.js, React Native, Git, and GitHub. Leveraging
+          these technologies, I've successfully completed various projects
+          including social media websites, e-commerce platforms, and numerous
+          other websites.
         </p>
         <p className="py-2 lg:leading-9 leading-7 text-[16px] lg:text-[18px]">
-          Additionally, I have also learned more technologies such as React.js
-          which is a JavaScript library for building user interfaces. Using this
-          library I have created many projects such as social media website,
-          e-commerce website and many more projects.
+          Currently, I work as a frontend developer, creating websites and
+          applications.
         </p>
       </div>
     </div>
